fb56775d56684c39180b121fc554c9b9a0a986f2
Serkan Bayram <serkanbay02@hotmail.com>
2024-11-20 14:41:27 +0100

    Order score data

--- src/controllers/score.ts
+++ src/controllers/score.ts
@@ -1,9 +1,13 @@
 import { Request, Response } from "express";
 import { db } from "../config/db.js";
 import { usersTable } from "../schemas/schema.js";
+import { desc } from "drizzle-orm";
 
 export async function getScores(req: Request, res: Response) {
-  const scores = await db.select().from(usersTable);
+  const scores = await db
+    .select()
+    .from(usersTable)
+    .orderBy(desc(usersTable.score));
 
   res.status(200).json({ scores: scores });
 }
